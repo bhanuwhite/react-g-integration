e5ef91a35220c379f01334f36242316296faeb2c
ubed <syedubaidmohiuddin@gmail.com>
2021-12-23 19:42:37 +0530

    added github profile

--- src/GithubApi/Github.jsx
+++ src/GithubApi/Github.jsx
@@ -1,50 +1,79 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
 import githubIMG from "../images/github.png";
-import "../dashboard/Dashboard.css"
-const Github = () => {                                           
-    return (      
-        <div>       
-            <h3>Github</h3>               
-            <div className='row'>                  
-                <div className='col-xl-4'>                                           
-                    <div className='git-details p-3'>
-                        <div className='d-flex  justify-content-between'>       
-                            <div className='user-name'>
-                                <h4>Robartray0125</h4>
-                                <span>robartray0125@gmail.com</span>
-                            </div>
-                            <div className='git-image'>
-                                <img src={githubIMG} alt="" />                                      
-                                
-                            </div>                                                     
-                        </div>
-                        <div className="cardDivider my-3"></div>
-                        <div className='gitrepo'>                                          
-                            <h5 className='border-bottom-2 mb-4'>Repositories :</h5>                     
-                            <div className='repo-name'>
-                                <h5><a href='#'>1. Robartray0125/Dashboard</a></h5>
-                                <h5><a href='#'>2. Robartray0125/UI</a></h5>
-                                <h5><a href='#'>3. Robartray0125/Facebook</a></h5>
-                                <h5><a href='#'>4. Robartray0125/React-g-integration</a></h5>
-                                <h5><a href='#'>5. Robartray0125/Rowlin.scss</a></h5>
-                                <h5><a href='#'>6. Robartray0125/gulp-project</a></h5>
-                            </div>
-                            <button            
-                                type="button"     
-                                className="btn btn-success mt-3"
-                                id="#"  
-                                >
-                                Open Github                                                                               
-                            </button>
-                        </div>
-                    </div>
-                       
-                                          
-                </div>
-               
-            </div>
-        </div>
-    )
-}
+import "../dashboard/Dashboard.css";
+import axios from "axios";
+import _ from "lodash";
+import { Button } from "react-bootstrap";
 
-export default Github
+const Github = () => {
+  const GITHUB_CLIENT_ID = "3d9a62acdac2de76bcb7";
+  const gitHubRedirectURL = "http://localhost:4000/api/auth/github";
+  const path = "/";
+
+  const [user, setUser] = useState("");
+  const [Repo, setRepo] = useState([]);
+
+  useEffect(() => {
+    (async function () {
+      const userDetail = await axios
+        .get(`http://localhost:4000/api/me`, {
+          withCredentials: true,
+        })
+        .then((res) => res.data);
+
+      console.log(userDetail, "userdetails");
+      axios.get(`${userDetail.repos_url}`).then((response) => {
+        console.log(response.data, "repo res");
+        setRepo(response.data);
+      });
+
+      setUser(userDetail);
+    })();
+  }, []);
+
+  return (
+    <div>
+      <h3>Github</h3>
+      <div className="row">
+        <div className="col-xl-4">
+          {" "}
+          <div className="git-details p-3">
+            <div>
+              <div className="App"></div>
+              <div className="d-flex  justify-content-between">
+                <div className="user-name">
+                  <h4> {user.name}</h4>
+                  <span>{user.login}</span>
+                </div>
+                <div className="git-image">
+                  <img src={githubIMG} alt="" />
+                </div>
+              </div>
+              <div className="cardDivider my-3"></div>
+              <div className="gitrepo">
+                <h5 className="border-bottom-2 mb-4">Repositories :</h5>
+                <div className="repo-name">
+                  <ol>
+                    {_.map(_.take(Repo, 4), (e, idx) => (
+                      <li>
+                        {idx + 1}-{e.name}
+                      </li>
+                    ))}
+                  </ol>
+                </div>
+                <button type="button" className="btn btn-success mt-3" id="#">
+                  <a href={`https://github.com/${user.login}?tab=repositories`}>
+                    {" "}
+                    Open Github
+                  </a>
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Github;
--- src/pages/settings/SettingPage.jsx
+++ src/pages/settings/SettingPage.jsx
@@ -3,8 +3,14 @@ import TwitterLogin from "../../accounts/TwitterLogin";
 import DriveLogin from "../../accounts/DriveLogin";
 import SideMenu from "../../components/sidemenu/SideMenu";
 import Header from "../../components/header/Header";
+import Github from "../../GithubApi/Github";
 
 const SettingPage = () => {
+
+  
+const GITHUB_CLIENT_ID = "3d9a62acdac2de76bcb7";
+const gitHubRedirectURL = "http://localhost:4000/api/auth/github";
+const path = "/";
   return (
     <>
       <div className="main-wrapper">
@@ -17,29 +23,42 @@ const SettingPage = () => {
               <hr />
             </div>
             <div className="container-fluid">
-              <div className="heading-inner ml-3">  
-                <h4 className="">Add social media Account</h4>                   
+              <div className="heading-inner ml-3">
+                <h4 className="">Add social media Account</h4>
               </div>
               <div className="row">
                 <div className="col-xl-6">
                   <div className="social-media-button">
                     <div className="col">
                       <DriveLogin />
-                    </div>                                         
-                  </div>      
-                </div>     
-                <div className="col-xl-6">              
+                    </div>
+                  </div>
+                </div>
+                <div className="col-xl-6">
                   <div className="social-media-button">
                     <div className="col">
                       <TwitterLogin />
                     </div>
                   </div>
-                </div>     
+                </div>
+                <div className="col-xl-6">
+                  <div className="social-media-button">
+                    <div className="col">
+                      <button type="button">
+                        <a
+                          href={`https://github.com/login/oauth/authorize?client_id=${GITHUB_CLIENT_ID}&redirect_uri=${gitHubRedirectURL}?path=${path}&scope=user:email`}
+                        >
+                          LOGIN WITH GITHUB
+                        </a>
+                      </button>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>                                  
+      </div>
     </>
   );
 };
